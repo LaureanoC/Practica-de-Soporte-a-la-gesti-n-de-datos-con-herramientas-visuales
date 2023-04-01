c1b7cabfc6ee13056949da4b8763c6000d94ff97
Laureano <laau262@gmail.com>
2023-04-01 04:15:25 -0300

    Resolución 4-5

---
+++ Practica 1/ejercicio4.py
@@ -0,0 +1,68 @@
+"""Expresiones Booleanas."""
+
+
+def es_vocal_if(letra: str) -> bool:
+    
+    """Toma un string y devuelve un booleano en base a si letra es una vocal o no.
+    Restricción: Utilizar un if para cada posibilidad con la función lower().
+    """
+    letra = letra.lower()
+    if(letra == 'a'):
+        return True
+   
+    if(letra == 'e'):
+        return True
+    
+    if(letra == 'i'):
+        return True
+    
+    if(letra == 'o'):
+        return True
+    
+    if(letra == 'u'):
+        return True
+    
+    return False
+
+
+# NO MODIFICAR - INICIO
+assert es_vocal_if("a")
+assert not es_vocal_if("b")
+assert es_vocal_if("A")
+# NO MODIFICAR - FIN
+
+
+###############################################################################
+
+
+def es_vocal_if_in(letra: str) -> bool:
+    """Re-escribir utilizando un sólo IF y el operador IN.
+    Referencia: https://docs.python.org/3/reference/expressions.html#membership-test-operations
+    """
+    letra = letra.lower()
+    if(letra in ('a','e','i','o','u')):
+        return True
+    return False
+
+# NO MODIFICAR - INICIO
+assert es_vocal_if_in("a")
+assert not es_vocal_if_in("b")
+assert es_vocal_if_in("A")
+# NO MODIFICAR - FIN
+
+
+###############################################################################
+
+
+def es_vocal_in(letra: str) -> bool:
+    """Re-escribir utilizando el operador IN pero sin utilizar IF."""
+    letra = letra.lower()
+    return letra in ('a','e','i','o','u')
+
+
+
+# NO MODIFICAR - INICIO
+assert es_vocal_in("a")
+assert not es_vocal_in("b")
+assert es_vocal_in("A")
+# NO MODIFICAR - FIN
