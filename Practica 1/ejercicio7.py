2f14b0eeee0cee7e080d6ea33a63035149d6081b
Laureano <laau262@gmail.com>
2023-04-03 20:24:04 -0300

    Ejercicio 7 Resuelto excepto el de slices, hecho con lambda

---
+++ Practica 1/ejercicio7.py
@@ -0,0 +1,62 @@
+"""Slicing."""
+
+
+from functools import reduce
+from math import ceil
+
+
+def es_palindromo(palabra: str) -> bool:
+    """Toma un string y devuelve un booleano en base a si se lee igual al
+    derecho y al revés.
+    Restricción: No utilizar bucles - Usar Slices de listas.
+    Referencia: https://docs.python.org/3/tutorial/introduction.html#lists
+    """
+   
+    if(palabra == ""):
+        return True
+    else:
+        print(reduce(lambda x,y: y + x, palabra)) ## No es con slices pero funciona 
+        return reduce(lambda x,y: y + x, palabra) == palabra
+
+
+
+# NO MODIFICAR - INICIO
+assert not es_palindromo("amor")
+assert es_palindromo("radar")
+assert es_palindromo("")
+# NO MODIFICAR - FIN
+
+
+###############################################################################
+
+
+def mitad(palabra: str) -> str:
+    """Toma un string y devuelve la mitad. Si la longitud es impar, redondear
+    hacia arriba.
+    Restricción: No utilizar bucles - Usar Slices de listas.
+    Referencia: https://docs.python.org/3/tutorial/introduction.html#lists
+    """
+
+
+    if(len(palabra) % 2 == 0):
+        
+        print(palabra)
+        print(palabra[0:2])
+        print(int(len(palabra)/2)) ## Sino me lo tomaba como 2.0 float
+        print(palabra[0:int(len(palabra)/2)])
+        return palabra[0:int(len(palabra)/2)]
+    else:
+
+        print(len(palabra)) # 5
+        print(len(palabra)/2)   # 2.5
+        print(ceil(len(palabra)/2))  #3
+        print(palabra[0:ceil(len(palabra)/2)]) ## Hel
+
+        return palabra[0:ceil(len(palabra)/2)]
+
+   
+# NO MODIFICAR - INICIO
+assert mitad("hello") == "hel"
+assert mitad("Moon") == "Mo"
+assert mitad("") == ""
+# NO MODIFICAR - FIN
