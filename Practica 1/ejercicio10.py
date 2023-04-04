51d60ff0b65dead78895f5237053e623e4bce289
Laureano <laau262@gmail.com>
2023-04-04 19:10:28 -0300

    Ejercicio 10 Sin la función de any()

---
+++ Practica 1/ejercicio10.py
@@ -0,0 +1,62 @@
+"""For-Else, Any."""
+
+from typing import Iterable
+
+
+def tiene_pares_basico(numeros: Iterable[int]) -> bool:
+    """Toma una lista y devuelve un booleano en función si tiene al menos un
+    número par."""
+    for numero in numeros:
+        if((numero % 2) == 0):
+            return True
+    return False
+
+# NO MODIFICAR - INICIO
+assert tiene_pares_basico([1, 3, 5]) is False
+assert tiene_pares_basico([1, 3, 5, 6]) is True
+assert tiene_pares_basico([1, 3, 5, 600]) is True
+# NO MODIFICAR - FIN
+
+
+###############################################################################
+
+
+def tiene_pares_for_else(numeros: Iterable[int]) -> bool:
+    """Re-Escribir utilizando for-else con dos return y un break.
+    Referencia: https://docs.python.org/3/tutorial/controlflow.html#break-and-continue-statements-and-else-clauses-on-loops
+    """
+
+    # Loop statements may have an else clause; it is executed when the loop terminates 
+    # through exhaustion of the iterable (with for) or when the condition becomes 
+    # false (with while), but not when the loop is terminated by a break statement.
+
+    for numero in numeros:
+        if((numero % 2) == 0):
+            return True
+    else:
+        return False
+
+
+# NO MODIFICAR - INICIO
+assert tiene_pares_for_else([1, 3, 5]) is False
+assert tiene_pares_for_else([1, 3, 5, 6]) is True
+assert tiene_pares_for_else([1, 3, 5, 600]) is True
+# NO MODIFICAR - FIN
+
+
+###############################################################################
+
+
+def tiene_pares_any(numeros: Iterable[int]) -> bool:
+    """Re-Escribir utilizando la función any, sin utilizar bucles.
+    Referencia: https://docs.python.org/3/library/functions.html#any
+    """
+
+    pass # Completar
+
+
+# NO MODIFICAR - INICIO
+assert tiene_pares_any([1, 3, 5]) is False
+assert tiene_pares_any([1, 3, 5, 6]) is True
+assert tiene_pares_any([1, 3, 5, 600]) is True
+# NO MODIFICAR - FIN
